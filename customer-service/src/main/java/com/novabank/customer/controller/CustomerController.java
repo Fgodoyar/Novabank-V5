25dfa45f966ad0b425015cc4909244402c4ad641
TKNFGOAR <frangoar22@gmail.com>
2026-05-13 18:53:01 +0200

    fix: Se eliminaron imports innecesarios

--- customer-service/src/main/java/com/novabank/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/novabank/customer/controller/CustomerController.java
@@ -8,14 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
